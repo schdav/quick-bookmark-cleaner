747e168f3d9c40d608bc019ccdc69c54a14b7333
David Schneider <schdav@users.noreply.github.com>
2022-05-19 13:13:40 +0200

    Update style

--- src/popup.js
+++ src/popup.js
@@ -1,2 +1,7 @@
-window.open(chrome.runtime.getURL('page.html'));
+if (chrome.runtime.openOptionsPage) {
+  chrome.runtime.openOptionsPage();
+} else {
+  window.open(chrome.runtime.getURL('page.html'));
+}
+
 window.close();
